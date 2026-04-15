5c31be36c17762155382fc014de277e110c3c023
Bryant Ferguson <btbf569708@gmail.com>
2026-04-15 14:09:19 -0400

    Finished jobseeker and Employer classes

--- backend/src/main/java/com/workforce/pipeline/model/JobSeeker.java
+++ backend/src/main/java/com/workforce/pipeline/model/JobSeeker.java
@@ -1,5 +1,25 @@
 package com.workforce.pipeline.model;
 
-public class JobSeeker {
+import com.workforce.pipeline.enums.UserRole;
+
+import java.util.ArrayList;
+
+public class JobSeeker extends User {
+    private ArrayList<Skill> listOfSkills;
+
+    public JobSeeker(String name, String email, String password, UserRole role) {
+        super(name, email, password, role);
+        this.listOfSkills = new ArrayList<>();
+    }
+
+    public void addSkill(Skill skill) {
+        listOfSkills.add(skill);
+    }
+    public void removeSkill(Skill skill) {
+        listOfSkills.remove(skill);
+    }
+    public ArrayList<Skill> getListOfSkills() {
+        return new ArrayList<>(listOfSkills);
+    }
 
 }
--- backend/src/main/java/com/workforce/pipeline/service/RecommendationService.java
+++ backend/src/main/java/com/workforce/pipeline/service/RecommendationService.java
@@ -1,4 +1,14 @@
 package com.workforce.pipeline.service;
 
+import com.workforce.pipeline.model.Job;
+
+import java.util.ArrayList;
+
 public class RecommendationService {
+/*
+    public ArrayList<Job> viewRecommendedJobs() {
+        return recommendationService.getRecommendedJobs(this);
+    }
+    */
+
 }
