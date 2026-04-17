df9b3be35c277b0c36351f96129244e39f3043ab
Bryant Ferguson <btbf569708@gmail.com>
2026-04-17 02:13:03 -0400

    Finished testing job files in postman

--- backend/src/main/java/com/workforce/pipeline/controller/TrainingController.java
+++ backend/src/main/java/com/workforce/pipeline/controller/TrainingController.java
@@ -1,5 +1,42 @@
 package com.workforce.pipeline.controller;
 
+import com.workforce.pipeline.model.Skill;
+import com.workforce.pipeline.model.TrainingProgram;
+import com.workforce.pipeline.service.TrainingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/training")
 public class TrainingController {
 
+    @Autowired
+    private TrainingService trainingService;
+
+    // CREATE PROGRAM
+    @PostMapping
+    public TrainingProgram create(@RequestBody TrainingProgram program) {
+        return trainingService.createTrainingProgram(program);
+    }
+
+    // GET ALL
+    @GetMapping
+    public List<TrainingProgram> getAll() {
+        return trainingService.getAllPrograms();
+    }
+
+    // GET BY ID
+    @GetMapping("/{id}")
+    public TrainingProgram getById(@PathVariable int id) {
+        return trainingService.getById(id);
+    }
+
+    @PostMapping("/{id}/skills")
+    public TrainingProgram addSkill(@PathVariable int id, @RequestBody Skill skill) {
+        TrainingProgram program = trainingService.getById(id);
+        program.addSkill(skill);
+        return trainingService.createTrainingProgram(program);
+    }
 }
